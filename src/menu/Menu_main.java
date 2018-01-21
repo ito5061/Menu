73eb07472ca4b3779e72894fe94f45c0d86a63f4
50616004 <50616004@50616004-pc>
2018-01-21 20:59:44 +0900

    Menu

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,16 @@
+package menu;
+
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Menu_view graph = new Menu_view();
+		graph.setBounds(5,5,755,555);
+		graph.setVisible(true);
+				
+
+	}
+
+}
